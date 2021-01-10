fc161aaac4951433ee50a91cc74a90a8880277e5
rockdeme <rockdeme@gmail.com>
2021-01-11 00:58:13 +0100

    new function to create training examples 1.0!

--- generate-input-for-u-net.py
+++ generate-input-for-u-net.py
@@ -6,70 +6,79 @@ from rbm.core.paras import PreParas, KerasParas
 from rbm.core.utils import min_max_normalization, resample_img, dim_2_categorical
 
 
-pre_paras = PreParas()
-pre_paras.patch_dims = [1, 128, 128]
-pre_paras.patch_label_dims = [1, 128, 128]
-pre_paras.patch_strides = [1, 32, 32]
-pre_paras.n_class = 2
-
-# Parameters for Keras model
-keras_paras = KerasParas()
-keras_paras.outID = 0
-keras_paras.thd = 0.5
-keras_paras.loss = 'dice_coef_loss'
-keras_paras.img_format = 'channels_last'
-keras_paras.model_path = os.path.join(os.getcwd(), 'rbm', 'scripts', 'rat_brain-2d_unet.hdf5')
+def preprocess(input_path):
+    imgobj = sitk.ReadImage(input_path)
+    # re-sample to 0.1x0.1x0.1
+    resampled_imgobj = resample_img(imgobj, new_spacing=[0.1, 0.1, 1], interpolator=sitk.sitkLinear)
+    print('Image resampled!')
+    img_array = sitk.GetArrayFromImage(resampled_imgobj)
+    img = min_max_normalization(img_array)
+    return img
 
 
+def create_training_examples(img):
+    pre_paras = PreParas()
+    pre_paras.patch_dims = [1, 128, 128]
+    pre_paras.patch_label_dims = [1, 128, 128]
+    pre_paras.patch_strides = [1, 32, 32]
+    pre_paras.n_class = 2
 
-input_path = 'C:/mri/sub-001_ses-1_anat_sub-001_ses-1_acq-RARE_T2w.nii.gz'
+    # Parameters for Keras model
+    keras_paras = KerasParas()
+    keras_paras.outID = 0
+    keras_paras.thd = 0.5
+    keras_paras.loss = 'dice_coef_loss'
+    keras_paras.img_format = 'channels_last'
+    keras_paras.model_path = os.path.join(os.getcwd(), 'rbm', 'scripts', 'rat_brain-2d_unet.hdf5')
 
-imgobj = sitk.ReadImage(input_path)
-# re-sample to 0.1x0.1x0.1
-resampled_imgobj = resample_img(imgobj, new_spacing=[0.1, 0.1, 0.1], interpolator=sitk.sitkLinear)
-print('Image resampled!')
-img_array = sitk.GetArrayFromImage(resampled_imgobj)
-img = min_max_normalization(img_array)
+    patch_dims = pre_paras.patch_dims
+    strides = pre_paras.patch_strides
+    length, col, row = img.shape
+    it = 0
+    z_dimension = len([*range(0, length-patch_dims[0]+1, strides[0])]) * len([*range(0, col-patch_dims[1]+1, strides[1])]) * len([*range(0, row-patch_dims[2]+1, strides[2])])
+    z_stack = np.zeros([z_dimension, patch_dims[1], patch_dims[2]])
+    for i in range(0, length-patch_dims[0]+1, strides[0]):
+        for j in range(0, col-patch_dims[1]+1, strides[1]):
+            for k in range(0, row-patch_dims[2]+1, strides[2]):
+                cur_patch=img[i:i+patch_dims[0],j:j+patch_dims[1],k:k+patch_dims[2]]
+                print(i,i+patch_dims[0],j,j+patch_dims[1],k,k+patch_dims[2])
+                z_stack[it,:,:] = cur_patch
+                it += 1
+                print(it)
+                if keras_paras.img_format == 'channels_last':
+                    z_stack = np.transpose(z_stack, (0, 2, 3, 1))
+    return z_stack
 
 
 
+X_input_path = 'C:/mri/X.nii'
+y_input_path = 'C:/mri/y.nii'
 
-patch_dims = pre_paras.patch_dims
+X_img = preprocess(X_input_path)
+y_img = preprocess(y_input_path)
+
+X = create_training_examples(X_img)
+
+
+
 label_dims = pre_paras.patch_label_dims
-strides = pre_paras.patch_strides
 n_class = pre_paras.n_class
 
 # build new variables for output
-length, col, row = img.shape
+
 categorical_map = np.zeros((n_class, length, col, row), dtype=np.uint8)
 likelihood_map = np.zeros((length, col, row), dtype=np.float32)
 counter_map = np.zeros((length,col,row), dtype=np.float32)
 length_step = int(patch_dims[0]/2)
 
-"""-----predict the whole image from two directions, small to large and large to small----"""
-img_array = np.zeros([(length-patch_dims[0]+1)*9,128,128])
-it = 0
-
-z_stack = len([*range(0, length-patch_dims[0]+1, strides[0])]) * len([*range(0, col-patch_dims[1]+1, strides[1])]) * len([*range(0, row-patch_dims[2]+1, strides[2])])
-
-for i in range(0, length-patch_dims[0]+1, strides[0]):
-    for j in range(0, col-patch_dims[1]+1, strides[1]):
-        for k in range(0, row-patch_dims[2]+1, strides[2]):
-            cur_patch=img[i:i+patch_dims[0],j:j+patch_dims[1],k:k+patch_dims[2]][:]
-            print(i,i+patch_dims[0],j,j+patch_dims[1],k,k+patch_dims[2])
-            img_array[it,:,:] = (cur_patch)
-            it += 1
-            print(it)
 
 
 
-                .reshape([1,patch_dims[0],patch_dims[1],patch_dims[2]])
 
 
-            if keras_paras.img_format == 'channels_last':
-                cur_patch = np.transpose(cur_patch, (0, 2, 3, 1))
 
-            cur_patch_output = cur_patch  # instead of predicting with the network I just assign the input as output
+
+
 
             # if there are multiple outputs
             if isinstance(cur_patch_output,list):
@@ -128,6 +137,6 @@ likelihood_map = np.divide(likelihood_map,counter_map)
 viewer = napari.Viewer()
 viewer.add_image(counter_map)
 viewer.add_image(categorical_map)
-viewer.add_image(likelihood_map)
-viewer.add_image(cur_patch)
-viewer.add_image(img_array)
+viewer.add_image(z_stack)
+viewer.add_image(X_img)
+viewer.add_image(y_img)
